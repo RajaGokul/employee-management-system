a87d244f69cbc043c32efc969cbc0432ad12eb31
RajaGokul <rajagokul11@gmail.com>
2026-07-25 13:59:40 +0530

    Initial Spring Boot Employee Management application

---
+++ src/main/java/com/devops/ems/service/EmployeeServiceImpl.java
@@ -0,0 +1,64 @@
+package com.devops.ems.service;
+
+import com.devops.ems.entity.Employee;
+import com.devops.ems.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(Long employeeId) {
+        return employeeRepository.findById(employeeId).orElse(null);
+    }
+
+    @Override
+    public Employee getEmployeeByEmail(String email) {
+        return employeeRepository.findByEmail(email);
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee updateEmployee(Long employeeId, Employee employee) {
+
+        Employee existingEmployee = employeeRepository.findById(employeeId).orElse(null);
+
+        if (existingEmployee != null) {
+
+            existingEmployee.setFirstName(employee.getFirstName());
+            existingEmployee.setLastName(employee.getLastName());
+            existingEmployee.setEmail(employee.getEmail());
+            existingEmployee.setDepartment(employee.getDepartment());
+            existingEmployee.setDesignation(employee.getDesignation());
+            existingEmployee.setSalary(employee.getSalary());
+            existingEmployee.setPhoneNumber(employee.getPhoneNumber());
+            existingEmployee.setStatus(employee.getStatus());
+            existingEmployee.setJoiningDate(employee.getJoiningDate());
+
+            return employeeRepository.save(existingEmployee);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        employeeRepository.deleteById(employeeId);
+    }
+
+}
